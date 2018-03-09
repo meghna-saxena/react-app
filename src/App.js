6d6c132b4c377e366ddf1903477285de2237f99c
Meghna Srivastava <meghna.s1906@gmail.com>
2018-03-09 16:26:06 +0100

    event handling with methods

--- src/App.js
+++ src/App.js
@@ -12,11 +12,18 @@ class App extends Component {
     ] 
   }
 
+//Using arrow function to overcome the problem of 'this', which is called by event handler
+  switchNameHandler = () => {
+    console.log('Was clicked!');
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a react app</h1>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button> 
+        {/* Dont pass parenthesis at the end of handler,otherwise it will immediately get rendered 
+        when this portion gets mount on DOM. Instead we just need a to pass a reference. */}
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Reacing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
