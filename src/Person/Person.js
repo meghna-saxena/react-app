06d6923221f17bb355ab0a21a1c50efbefdbf9ab
Meghna Srivastava <meghna.s1906@gmail.com>
2018-03-12 13:33:29 +0100

    styling component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Person.css';
 
 //If we want to run some dynamic content as JS and not interpret as text, wrap it in {}
 //props is an object which gives access to all the properties/attributes passed to the component
 
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old!</p>
             <p>{props.children}</p> 
            {/* props.children refers to any element b/w opening and closing tag of component */}
