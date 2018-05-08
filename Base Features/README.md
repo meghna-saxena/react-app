2295f9acf1bc11e22910a1183f49f5e5ac0b9617
Meghna Srivastava <meghna.s1906@gmail.com>
2018-05-08 07:33:22 +0200

    revising this section

--- Base Features/README.md
+++ Base Features/README.md
@@ -1,2 +1,71 @@
-# react-app
-Personal notes and project on React.
+# Base-Features
+ 
+## Using a workflow
+
+### Why?
+- Recommended for SPAs and multi-page apps (MPAs)
+- Optimize code -> increases performance of app
+- Use next-gen JS features
+- Be more productive -> by including features like CSS autoprefixing
+* CSS prefixes are used to achieve boradest possible borwser support for CSS features. But manually adding them is cumbersome.
+- Linting tool
+
+
+### How?
+- Use dependency management tool. Eg: npm or yarn
+- Use bundler. Recommended: Webpack. It doesnt just bundles all the files, but also allows to apply couple of other build steps before it does bundling. Eg: Compilation of next-gen JS.
+- Use compiler (next-gen javascript). Babel + presets
+- Use development server
+
+## What does extends Component mean?
+The class uses the extends keyword to inherit the class/object from react library to access its properties and methods.
+
+```
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>I'm a React App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
+```
+
+## Understanding JSX
+- We need to import react in each fie where we're returning JSX. Since JSX gets transpiled to React.createElement() at the end.
+- Usually React.createElement() takes 3 arguments: 
+1. Element we want to render on DOM. Eg: <div> or some component
+2. Second arg is configuration for it. Optional, so we can pass null, if we dont want to configure it.
+3. Third arg is any amt of children. We can have multiple arg separated by commas. Children means what's nested inside <div>
+
+> See how JSX gets compiled into JS
+
+```
+class App extends Component {
+  render() {
+    // return (
+    //   <div className="App">
+    //     <h1>I'm a React App!</h1>
+    //   </div>
+    // );
+
+    //  return React.createElement('div', null, 'h1', 'I\'m a React App!'); //here h1 is interpreted as text, not as element
+    
+    // return React.createElement('div', null, React.createElement('h1', null, 'I\'m a React App!')); 
+    //text gets rendered properly but no css class applied
+
+     return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m a React App!')); 
+     //correctly rendered
+  }
+}
+
+export default App;
+```
+
+
+## JSX restrictions
+- use className
+- When returning JSX inside a component, JSX expression must have one root element. Wrap everything inside one root element i.e <div>
--- Base Features/src/App.js
+++ Base Features/src/App.js
@@ -1,82 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person';
 
 class App extends Component {
-  //New ES6+ enables to use state propert directly without enclosing it in constructor function
-  state = {
-    persons: [
-      { name:'Max', age:28 },
-      { name:'Mike', age:25 },
-      { name:'Neil', age:29 }
-    ] 
-  }
-
-//Using arrow function to overcome the problem of 'this', which is called by event handler
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked!');
-    //DON'T mutate/change state directly => this.state.persons[0].name = 'Shane';
-    //use setState method by Component object imported by react
-
-    this.setState({
-      persons: [
-        { name:newName, age:28 },
-        { name:'John', age:25 },
-        { name:'Karl', age:29 }   
-      ] 
-    })
-  }
-
-  nameChangeHandler = (event) => {
-    this.setState({
-      persons: [
-        { name:'Max', age:28 },
-        { name:event.target.value, age:25 },
-        { name:'Karl', age:29 }   
-      ] 
-    })
-  }
-
   render() {
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    }
-
     return (
       <div className="App">
-        <h1>Hi, I'm a react app</h1>
-        <button 
-        style={style}
-        onClick={() => this.switchNameHandler('Shane')}>Switch Name</button> 
-        {/* Dont pass parenthesis at the end of handler,otherwise it will immediately get rendered 
-        when this portion gets mount on DOM. Instead we just need a to pass a reference. */}
-
-        {/* Even though parenthesis is used after handler's name, but this wont get executed immediately. */}
-        {/* Instead it's a anonymous func. which gets executed on a click */}
-
-        {/* Although a convenient syntax, but can be INEFFECIENT.
-        React can rerender certain things. */}
-        <Person 
-        name={this.state.persons[0].name} 
-        age={this.state.persons[0].age} />
-
-        <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age}
-        //passing method references as props between components
-        //MOST EFFECIENT WAY TO PASS DATA IS BY BINDING `this` to the specific function of this class
-        click={this.switchNameHandler.bind(this, 'Max')}
-        changed={this.nameChangeHandler}>
-        My hobbies: Racing</Person>
-
-        <Person 
-        name={this.state.persons[2].name} 
-        age={this.state.persons[2].age} />
-      </div>  
+        <h1>I'm a React App!</h1>
+      </div>
     );
   }
 }
