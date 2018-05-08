58188f433824e3d643b3ca8757381370e9ef916b
Meghna Srivastava <meghna.s1906@gmail.com>
2018-05-08 16:00:07 +0200

    rendering content conditionally

--- ListsConditions/README.md
+++ ListsConditions/README.md
@@ -1,2 +1,8 @@
-# react-app
-Personal notes and project on React.
+# Rendering Lists & Conditional Content
+Creating truly dynamic apps
+
+## How to output dynamic list
+
+Note: if/else cant be used inside JSX. Only simple statements can be written, no block statements. So use ternary expressions.
+
+- - For outputting any dynamic content, wrap it in curly braces, even if thats a div containing list of components. Javascript expressions can be injected inside jsx with {}.
--- ListsConditions/src/App.js
+++ ListsConditions/src/App.js
@@ -1,53 +1,40 @@
 import React, { Component } from 'react';
+import Person from './Person';
 import './App.css';
-import Person from './Person/Person';
 
 class App extends Component {
-  //New ES6+ enables to use state propert directly without enclosing it in constructor function
   state = {
     persons: [
-      { id: 'abc', name: 'Max', age: 28 },
-      { id: 'def', name: 'Mike', age: 25 },
-      { id: 'ghi', name: 'Neil', age: 29 }
-    ],
-    showPersons: false
+      { name: "Meg", age: 28 },
+      { name: "Raj", age: 29 },
+      { name: "Max", age: 30 }
+    ]
   }
 
-  nameChangeHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex(p => {
-      return p.id === id;
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: "Raj", age: 29 },
+        { name: "Max", age: 32 }
+      ]
     });
-
-    const person = {
-      //everything in react is JS object
-      //spread operator is pulling out old array and adding new array to it
-      //since arrays are reference types they refer the pointer to original object
-      //hence mutate the state
-      ...this.state.persons[personIndex]
-    };
-
-    person.name = event.target.value;
-
-    const persons = [...this.state.persons];
-    persons[personIndex] = person;
-
-    this.setState({ persons: persons });
   }
 
-  deletePersonsHandler = (personIndex) => {
-    // change state immutably
-    // First approach -> const persons = this.state.persons.slice();
-    //Another approach
-    const persons = [...this.state.persons];
-    // splice removes on element from the array
-    persons.splice(personIndex, 1);
-    this.setState({ persons: persons });
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: "Meggie", age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: "Max", age: 30 }
+      ],
+      showPersons: false
+    });
   }
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    //if doesShow is true, showPersons is false and vice versa
-    this.setState({ showPersons: !doesShow });
+    this.setState({showPersons: !doesShow});
   }
 
   render() {
@@ -57,33 +44,32 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer'
-    }
-
-    let persons = null;
-
-    if (this.state.showPersons) {
-      persons = (
-        <div>
-          {/* outputting lists */}
-          {this.state.persons.map((person, index) => {
-            return <Person
-              click={() => this.deletePersonsHandler(index)}
-              name={person.name}
-              age={person.age}
-              key={person.id}
-              changed={(event) => this.nameChangeHandler(event, person.id)} />
-          })}
-        </div>
-      );
     };
 
     return (
       <div className="App">
-        <h1>Hi, I'm a react app</h1>
+        <h1>I'm a React App!</h1>
         <button
           style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        {persons}
+          onClick={this.togglePersonsHandler}>Switch Name
+        </button>
+        {this.state.showPersons ?
+          <div>
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age} />
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              clicked={this.switchNameHandler.bind(this, 'Ed')}
+              changed={this.nameChangedHandler}>
+              Hobbies: Playing video games
+        </Person>
+            <Person
+              name={this.state.persons[2].name}
+              age={this.state.persons[2].age} />
+          </div> : null
+        }
       </div>
     );
   }
